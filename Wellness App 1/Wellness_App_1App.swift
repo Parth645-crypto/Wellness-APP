f05d6a399dc28bc1c47b5c11f0e4bd42ef116a77
Parth Lakhotiya <parth.lakhotiya645@gmail.com>
2026-02-06 15:15:59 +0530

    Initial Commit

---
+++ Wellness App 1/Wellness_App_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Wellness_App_1App.swift
+//  Wellness App 1
+//
+//  Created by SDC-USER on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Wellness_App_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
